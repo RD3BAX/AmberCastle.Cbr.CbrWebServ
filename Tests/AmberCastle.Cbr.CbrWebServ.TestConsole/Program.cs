5524b11066c9879bc6558385004866b5448204c8
Magistr <MagistrTNT@msn.com>
2021-07-11 13:51:54 +0300

    Подготовка тестовых проектов

--- Tests/AmberCastle.Cbr.CbrWebServ.TestConsole/Program.cs
+++ Tests/AmberCastle.Cbr.CbrWebServ.TestConsole/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace AmberCastle.Cbr.CbrWebServ.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            //.ConfigureAppConfiguration((hostContext, builder) =>
+            //{
+            //    // Add other providers for JSON, etc.
+
+            //    if (hostContext.HostingEnvironment.IsDevelopment())
+            //    {
+            //        builder.AddUserSecrets<Program>();
+            //    }
+            //})
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
-            Console.WriteLine("Hello World!");
+            //services.AddHttpClient<CbrClient>(client =>
+            //{
+            //    var config = host.Configuration.GetSection("CbrWebServ");
+            //    client.BaseAddress = new Uri(
+            //        $"{config["Schema"]}://" +
+            //        $"{config["Address"]}"
+            //        );
+            //})
+            //    .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+            //    .AddPolicyHandler(GetRetryPolicy())
+            //    ;
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            var jitter = new Random();
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(6, retry_attempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retry_attempt)) +
+                    TimeSpan.FromMilliseconds(jitter.Next(0, 1000)));
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            
+
+            Console.WriteLine("Завершено!");
+            Console.ReadLine();
+            await host.StopAsync();
         }
     }
 }
