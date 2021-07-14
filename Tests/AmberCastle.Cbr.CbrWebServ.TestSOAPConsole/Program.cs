d2bb4b542b7ef946588ccaec56fda8ca073b4b29
Magistr <MagistrTNT@msn.com>
2021-07-14 21:09:49 +0300

    Тестовая реализация класса работы с WebServer ЦБ с помощью специального метода эмуляции SOAP 1.2

--- Tests/AmberCastle.Cbr.CbrWebServ.TestSOAPConsole/Program.cs
+++ Tests/AmberCastle.Cbr.CbrWebServ.TestSOAPConsole/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -16,89 +17,47 @@ namespace AmberCastle.Cbr.CbrWebServ.TestSOAPConsole
         {
             Console.WriteLine("Hello World!");
 
-            //var method = "GetCursOnDate";
-            //var _params = "On_date";
             XNamespace myns = "http://web.cbr.ru/";
-            //XElement parameters = new XElement(myns + method,
-            //    //new XElement(myns + "client",
-            //    //    new XElement(myns + "Username", $"_apiUsername"),
-            //    //    new XElement(myns + "Password", $"_apiPassword")),
-
-            //    new XElement(myns + _params, DateTime.Today)
-            //);
 
             XElement parameters = 
                 new XElement(myns + "OstatDynamic",
                     new XElement(myns + "fromDate", DateTime.Today.AddDays(-10)),
                     new XElement(myns + "ToDate", DateTime.Today)
+                );
+            
+            
+            XElement parameters2 = 
+                new XElement(myns + "GetLatestDateTime",
+                    new XElement(myns + "fromDate", DateTime.Today.AddDays(-10)),
+                    new XElement(myns + "ToDate", DateTime.Today)
+                );
 
-            );
+
+            var connection_string = $"https://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx";
 
 
-            var _apiUrl = $"http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx";
-
-            XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
-            XNamespace xsd = "http://www.w3.org/2001/XMLSchema";
-            XNamespace ns = "http://www.w3.org/2003/05/soap-envelope";
-
-            XDocument soapRequest = new XDocument(
-                new XDeclaration("1.0", "UTF-8", "no"),
-                new XElement(ns + "Envelope",
-                    new XAttribute(XNamespace.Xmlns + "xsi", xsi),
-                    new XAttribute(XNamespace.Xmlns + "xsd", xsd),
-                    new XAttribute(XNamespace.Xmlns + "soap", ns),
-                    new XElement(ns + "Body", parameters)
-                ));
-            //XElement body = soapRequest.Element("Body");
-            //body.Add(new XElement(myns + method,
-            //    //new XElement(myns + "client",
-            //    //    new XElement(myns + "Username", $"_apiUsername"),
-            //    //    new XElement(myns + "Password", $"_apiPassword")),
-
-            //    new XElement(myns + _params, DateTime.Today)
-            //));
-
-            //soapRequest.Element(ns + "Envelope").Element(ns + "Body").Add(new XElement(myns + method,
-            //    //new XElement(myns + "client",
-            //    //    new XElement(myns + "Username", $"_apiUsername"),
-            //    //    new XElement(myns + "Password", $"_apiPassword")),
-
-            //    new XElement(myns + _params, DateTime.Today)
-            //));
-
-            //soapRequest.Element(ns + "Envelope").Element(ns + "Body").Add(parameters);
 
             try
             {
-                using (var client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip }) /*{ Timeout = _timeout }*/)
+                using (var client = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip })
                 {
-                    var request = new HttpRequestMessage()
-                    {
-                        RequestUri = new Uri(_apiUrl),
-                        Method = HttpMethod.Post
-                    };
+                    BaseAddress = new Uri(connection_string)
+                    //Timeout = _timeout
+                })
+                {
+                    var cbrClient = new CbrСlient(client);
 
-                    request.Content = new StringContent(soapRequest.ToString(), Encoding.UTF8, "text/xml");
+                    //var result = cbrClient.GetFromCbr(parameters).Result;
+                    
+                    //Console.WriteLine(result);
 
-                    request.Headers.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
-                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
-                    //request.Headers.Add("SOAPAction", $"http://web.cbr.ru/{method}");
+                    //var result2 = cbrClient.GetFromCbr(parameters2).Result;
 
-                    var test = request.ToString();
+                    //Console.WriteLine(result2);
 
-                    HttpResponseMessage response = client.SendAsync(request).Result;
+                    Console.WriteLine(cbrClient.AllDataInfoXML().Result);
 
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        throw new Exception();
-                    }
 
-                    Task<Stream> streamTask = response.Content.ReadAsStreamAsync();
-                    Stream stream = streamTask.Result;
-                    var sr = new StreamReader(stream);
-                    var soapResponse = XDocument.Load(sr);
-                    Console.WriteLine(soapResponse);
 
                     //var xml = soapResponse.Descendants(myns + "GetStuffResult").FirstOrDefault().ToString();
                     //var purchaseOrderResult = StaticMethods.Deserialize<GetStuffResult>(xml);
@@ -120,5 +79,107 @@ namespace AmberCastle.Cbr.CbrWebServ.TestSOAPConsole
                 throw ex;
             }
         }
+    }
+
+    public class CbrСlient
+    {
+        #region Поля
+
+        private readonly HttpClient _Client;
+
+        #endregion // Поля
+
+        #region Методы
+
+        /// <summary>
+        /// Получение всей оперативной информации (XmlDocument)
+        /// </summary>
+        /// <returns></returns>
+        public async Task<XDocument> AllDataInfoXML(CancellationToken Cancel = default)
+        {
+            XNamespace myns = "http://web.cbr.ru/";
+            XElement parameters =
+                new XElement(myns + "AllDataInfoXML");
+
+            return await GetFromCbr(parameters, Cancel).ConfigureAwait(false);
+        }
+
+        #endregion // Методы
+
+        public async Task<XDocument> GetFromCbr(XElement parameters, CancellationToken Cancel = default)
+        {
+            //var _apiUrl = $"/DailyInfoWebServ/DailyInfo.asmx";
+
+            XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+            XNamespace xsd = "http://www.w3.org/2001/XMLSchema";
+            XNamespace ns = "http://www.w3.org/2003/05/soap-envelope";
+
+            XDocument soapRequest = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "no"),
+                new XElement(ns + "Envelope",
+                    new XAttribute(XNamespace.Xmlns + "xsi", xsi),
+                    new XAttribute(XNamespace.Xmlns + "xsd", xsd),
+                    new XAttribute(XNamespace.Xmlns + "soap12", ns),
+                    new XElement(ns + "Body", parameters)
+                ));
+
+
+            try
+            {
+
+                var request = new HttpRequestMessage()
+                {
+                    //RequestUri = new Uri(_apiUrl),
+                    Method = HttpMethod.Post
+                };
+
+                request.Content = new StringContent(soapRequest.ToString(), Encoding.UTF8, "text/xml");
+
+                request.Headers.Clear();
+                _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
+                //request.Headers.Add("SOAPAction", $"http://web.cbr.ru/{method}");
+
+                var test = request.ToString();
+
+                HttpResponseMessage response = await _Client.SendAsync(request, Cancel).ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    //throw new Exception();
+                    return default;
+                }
+
+                Task<Stream> streamTask = response.Content.ReadAsStreamAsync(Cancel);
+                Stream stream = await streamTask.ConfigureAwait(false);
+                var sr = new StreamReader(stream);
+
+                return XDocument.Load(sr);
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerException is TaskCanceledException)
+                {
+                    throw ex.InnerException;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        #region Конструктор
+
+        public CbrСlient(HttpClient Client)
+        {
+            _Client = Client;
+        }
+
+        #endregion // Конструктор
     }
 }
